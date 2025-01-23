ed0b0ae43ea1f5889c4a080dab82a9c397073b8b
dhomanhurimtm <dhomanhuri.ilyas@mtm.id>
2025-01-23 06:35:22 +0000

    done to set host as ip

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const { Client } = require('ssh2');
 const http = require('http');
 const socketIo = require('socket.io');
 
+const dns = require('dns');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -15,38 +16,45 @@ const io = socketIo(server, {
 
 app.use(cors());
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) =>  {
     const conn = new Client();
 
-    socket.on('connectSSH', (query) => {
-        const { host, port, username, password } = query;
-
+    socket.on('connectSSH',  (query) => {
+        var { host, port, username, password } = query;
+        
+        
         if (!host || !port || !username || !password) {
             socket.emit('error', 'Missing required query parameters');
             return;
         }
-
-        conn.on('ready', () => {
-            socket.emit('status', 'Connected to SSH');
-            conn.shell((err, stream) => {
-                if (err) {
-                    socket.emit('error', 'Failed to open shell');
-                    return;
-                }
-                socket.on('input', (data) => stream.write(data));
-                stream.on('data', (data) => socket.emit('output', data.toString()));
-                stream.stderr.on('data', (data) => socket.emit('output', data.toString()));
-            });
-        }).connect({
-            host,
-            port: parseInt(port, 10),
-            username,
-            password,
-        });
-
-        conn.on('error', (err) => {
-            socket.emit('error', `SSH connection error: ${err.message}`);
-        });
+            dns.lookup(host,(err, addresses, family) => {
+                host = addresses;
+                console.log(host)
+                console.log("error")
+                console.log(err)
+                conn.on('ready', () => {
+                    socket.emit('status', 'Connected to SSH');
+                    conn.shell((err, stream) => {
+                        if (err) {
+                            socket.emit('error', 'Failed to open shell');
+                            return;
+                        }
+                        socket.on('input', (data) => stream.write(data));
+                        stream.on('data', (data) => socket.emit('output', data.toString()));
+                        stream.stderr.on('data', (data) => socket.emit('output', data.toString()));
+                    });
+                }).connect({
+                    host,
+                    port: parseInt(port, 10),
+                    username,
+                    password,
+                });
+        
+                conn.on('error', (err) => {
+                    socket.emit('error', `SSH connection error: ${err}`);
+                });
+            })
+        
     });
 
     socket.on('disconnect', () => {
